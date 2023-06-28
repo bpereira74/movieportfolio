463ba4fb613b847d41d1b11f7f331938be527305
bpereira74 <bpereira74@hotmail.com>
2023-06-27 23:31:11 -0400

    feat: Cracon de route

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root 'movie#index'
   # root "articles#index"
 end
