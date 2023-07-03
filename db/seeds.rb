ffa0b9209b5afc2600e126dfc130b937683f2da2
bpereira74 <bpereira74@hotmail.com>
2023-07-02 20:10:34 -0400

    feat: Creación de seeds para poblar las BD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |i|
+      Movies.create(name: "Name #{i}", synopsis: "Synopsis #{i}", director: " #{i}")
+      Series.create(name: "Name #{i}", synopsis: "Synopsis #{i}", director: " #{i}")
+      Documentaryfilms.create(name: "Name #{i}", synopsis: "Synopsis #{i}", "director: #{i}")
+    end
